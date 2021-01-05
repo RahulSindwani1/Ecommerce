2688ac5713e36a542253a42d777c3abb426c6fdd
Rahul Sindwani <rahulsindwani435@gmail.com>
2021-01-05 01:09:12 -0600

    Product Screen added

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
         <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
           <Container>
             <LinkContainer to="/">
-              <Navbar.Brand >Hari Electronics</Navbar.Brand>
+              <Navbar.Brand > <strong>Hari Electronics </strong></Navbar.Brand>
               </LinkContainer>
               <Navbar.Toggle aria-controls="basic-navbar-nav" />
               <Navbar.Collapse id="basic-navbar-nav">
